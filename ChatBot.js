c331276d81a8465e5b2e07bffba7e908e870c7f2
Nagaraj Penagondala <nagaraj.sp15@gmail.com>
2024-12-30 13:57:48 +0530

    adding new developing project

---
+++ ChatBot.js
@@ -0,0 +1,67 @@
+
+const chatBody = document.querySelector(".chat-body");
+const inputMsg = document.querySelector(".input-message");
+const sendMsgBtn = document.querySelector("#send-message");
+
+//const API_KEY = "AIzaSyAV-eNz8AnD54BA0QU26OuI5THT-_yvM0k";
+const API_url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBU4vlKdV50l2X7tOMo7sHjsrJZ3tr1tq8`;
+
+const userData = {
+    message: null
+}
+
+const handleOutgoingMsg = (e)=>{
+    e.preventDefault();
+    userData.message = inputMsg.value.trim();
+    inputMsg.value="";
+    const div = document.createElement("div");
+    div.classList.add("user-side-message");
+    div.textContent = userData.message;
+    chatBody.appendChild(div);
+
+    setTimeout(()=>{
+        const botDiv = document.createElement("div");
+        botDiv.classList.add("bot-side-message");
+        botDiv.innerHTML = `<img src="bot.png" alt="" class="bot-avatar">
+                <div class="bot-message">
+                    <div class="think">
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                    </div>
+                </div>`;
+                chatBody.appendChild(botDiv);
+                chatBody.scrollTop = chatBody.scrollHeight;
+    }, 500)
+    chatBody.scrollTop = chatBody.scrollHeight;
+};
+
+const generateBotResponse = async ()=>{
+    try {
+        const response = await fetch(API_url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                contents: [{
+                    parts: [{ text: userData.message}]
+                }]
+            })
+        });
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.error.message)
+            console.log(data);
+    } catch (error){
+        console.log(error)
+    }
+}
+generateBotResponse();
+inputMsg.addEventListener("keydown", (e)=>{
+    const textMsg = e.target.value.trim();
+    if(e.key==="Enter" && textMsg){
+        handleOutgoingMsg(e);
+    }
+});
+
+sendMsgBtn.addEventListener("click", (e)=>{
+    handleOutgoingMsg(e);
+});
